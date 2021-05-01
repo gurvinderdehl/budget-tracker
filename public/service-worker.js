14194163578aa151bf650e28a68328a28465052f
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-05-01 22:16:39 +0100

    SW function to fetch app

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,3 +41,35 @@ self.addEventListener('activate', function (event) {
     );
     self.clients.claim();
 });
+
+
+//This is fetching the app
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
