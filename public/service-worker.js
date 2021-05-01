19a94cd6239157c61f49c337205bdb4ed57c5feb
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-05-01 22:13:54 +0100

    SW function to activate app added

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,14 +14,30 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 //Installing my app
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
 
     event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(FILES_TO_CACHE);
-        })
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(FILES_TO_CACHE);
+            })
     );
     self.skipWaiting();
-  });
+});
+//Activating the app with function below:
+self.addEventListener('activate', function (event) {
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (CACHE_NAME.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
