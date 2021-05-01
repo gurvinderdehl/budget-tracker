ff9bc6953b704438c5a4cbc47c65f932862bcac9
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-05-01 21:37:02 +0100

    Update db.js

--- public/db.js
+++ public/db.js
@@ -35,9 +35,34 @@ request.onupgradeneeded = function (event) {
   function checkDatabase() {
     db = request.result; // is this right?
     // open a transaction on your pending db
-    const transaction = db.transaction(["budget"], "readwrite");
+    const transaction = db.transaction(["pending"], "readwrite");
     // access your pending object store
-    const budgetStore = transaction.objectStore("budget");
+    const budgetStore = transaction.objectStore("pending");
     // get all records from store and set to a variable
     const getAll = store.getAll();
     const budgetIndex = budgetStore.index("budgetIndex");
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+          fetch('/api/transaction/bulk', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': 'application/json',
+            },
+          })
+            .then((response) => response.json())
+            .then(() => {
+              // if successful, open a transaction on your pending db
+              const transaction = db.transaction(["pending"], "readwrite");
+              // access your pending object store
+              const store = transaction.objectStore("pending");
+              // clear all items in your store
+              store.clear();
+            });
+        }
+      };
+    }
+    // listen for app coming back online
+    window.addEventListener('online', checkDatabase);
